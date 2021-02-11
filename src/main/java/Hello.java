f855bda8538a78b036926daf61bdea5eb0f4f301
erenduygu <erenduygu33@gmail.com>
2021-02-11 18:13:28 +0300

    Master branch spouse change team number

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world B22");
         System.out.println("EU4 is great team");
         System.out.println("again one more comment");
     }
