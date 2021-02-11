9a40dc22cd5c1fc9925fb3152c72df0099f1e3e0
erenduygu <erenduygu33@gmail.com>
2021-02-11 18:12:38 +0300

    Watching more movie

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        System.out.println("hello world EU4");
         System.out.println("EU4 is great team");
         System.out.println("again one more comment");
+        // Watching more movie
     }
 }
