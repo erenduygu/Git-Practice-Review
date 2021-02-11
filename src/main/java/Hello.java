b6ecebb7fc31f424b465df3e79589a1c61859d89
erenduygu <erenduygu33@gmail.com>
2021-02-11 18:19:43 +0300

    merge not knowing your spouses action

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("hello world EU4");
+        System.out.println("hello world 22");
         System.out.println("EU4 is great team");
         System.out.println("again one more comment");
         // Watching more movie
+
+        //merge not knowing your spouses action
     }
 }
