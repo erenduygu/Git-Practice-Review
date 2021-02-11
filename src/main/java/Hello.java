a2a639db3fe622ff3eb89e6df7d1f092c0a6b636
erenduygu <erenduygu33@gmail.com>
2021-02-11 12:28:03 +0300

    Added new text

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("EU4 is great team");
+        System.out.println("again one more comment");
     }
 }
