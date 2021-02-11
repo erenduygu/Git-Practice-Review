b186a5f8b797a862653dc5d394d00b279aa689b8
erenduygu <erenduygu33@gmail.com>
2021-02-11 19:07:40 +0300

    this is new work

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world B22");
+
+        System.out.println("hello world 22");
         System.out.println("EU4 is great team");
         System.out.println("again one more comment");
+        // Watching more movie
+
+        //merge not knowing your spouses action
     }
 }
